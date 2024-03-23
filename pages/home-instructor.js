c10e0dbc1872ba030c19bfd37b99aa982fab24bb
dooodle74 <dpwang01@gmail.com>
2024-03-23 12:59:55 -0400

    home pages

---
+++ pages/home-instructor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const InstructorHome = () => {
+  return (
+    <main>
+      <div>
+      <h1>Instructor Homepage</h1>
+      <p>Welcome to the instructor homepage!</p>
+      {/* Add any instructor-specific content or functionality here */}
+      </div>
+    </main>
+  );
+};
+
+export default InstructorHome;
---
+++ pages/home-user.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserHome = () => {
+  return (
+    <div>
+      <h1>User Homepage</h1>
+      <p>Welcome to the user homepage!</p>
+      {/* Add any user-specific content or functionality here */}
+    </div>
+  );
+};
+
+export default UserHome;
